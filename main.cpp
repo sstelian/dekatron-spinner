b95116af2a046ab0084745f0bbcc8d16f7f60502
Stelian Saracut <steliansaracut@gmail.com>
2023-04-16 07:54:14 +0300

    updated README

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /* 
- * Dekatron spinner for the OG-4 tube. The code provides some functions for
+ * Dekatron spinner for the OG-4 tube based on mbed. The code provides some functions for
  * incrementing, reading and writing the dekatron. Recommended target: LPC1768.
  * 
  * Stelian Saracut 2023
